c6251e418bc717fafa86191fe65c66a825576632
Abdullah Al Amin <aaa006bd@gmail.com>
2017-09-26 15:54:00 +0600

    relief record class updated

---
+++ src/main/java/org/fteller/model/relief/Organization.java
@@ -0,0 +1,7 @@
+package org.fteller.model.relief;
+
+/**
+ * Created by Abdullah Al Amin on 9/26/2017.
+ */
+public class Organization {
+}
--- src/main/java/org/fteller/model/relief/ReliefRecords.java
+++ src/main/java/org/fteller/model/relief/ReliefRecords.java
@@ -1,7 +1,29 @@
 package org.fteller.model.relief;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.fteller.model.areas.UnionParisad;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Set;
+
 /**
  * Created by Abdullah Al Amin on 9/26/2017.
  */
+@NoArgsConstructor
+
 public class ReliefRecords {
+
+    private @Getter@Setter int id;
+    private  @Setter LocalDateTime timestamp ;
+    private @Getter@Setter UnionParisad place;
+    private Set<Organization> organizations;
+
+
+    public LocalDate getDate(){
+       return timestamp.toLocalDate();
+    }
+
 }
